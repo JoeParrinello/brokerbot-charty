972c42b4c9454f72ac6ab651711d0a3ec6678214
Joe Parrinello <joe@parrinello.me>
2022-09-28 22:24:06 +0000

    Adding skeleton for charty code

---
+++ main.py
@@ -0,0 +1,49 @@
+from google.cloud import storage
+from flask import make_response
+import functions_framework
+import os
+import uuid
+import plotly.graph_objects as go
+import pandas as pd
+
+def get_bucket_name() -> str:
+    return os.environ.get('CLOUD_STORAGE_BUCKET_NAME', 'my-bucket-id')
+
+def upload_blob_and_return_url(chart_local_file_name: str):
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(get_bucket_name())
+    blob = bucket.blob(chart_local_file_name)
+
+    blob.upload_from_filename(chart_local_file_name, content_type='image/png')
+
+    blob.make_public()
+
+    return blob.public_url
+
+def create_chart_name() -> str:
+    return "chart_{}.png".format(str(uuid.uuid4()));
+
+def convert_dataframe_to_chart(df: pd.DataFrame) -> str:
+    fig = go.Figure(data=[go.Candlestick(x=df['Timestamp'],
+                open=df['Open'], high=df['High'],
+                low=df['Low'], close=df['Close'])
+                     ])
+
+    fig.update_layout(xaxis_rangeslider_visible=False)
+
+    chart_name = create_chart_name()
+    fig.write_image(chart_name, format='png', width=300, height=200)
+    return chart_name
+
+@functions_framework.http
+def get_ticker_graph(request):
+
+  request_json = request.get_json(silent=True)
+  if request_json is None:
+    return make_response("Bad request JSON", 400)
+  
+  df = pd.DataFrame(request_json)
+  chart_name = convert_dataframe_to_chart(df)
+  chart_url = upload_blob_and_return_url(chart_name)
+
+  return chart_url
